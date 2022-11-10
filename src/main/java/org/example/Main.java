d4c6a4eef236a48063ddc2265c12029654a8acc3
chenjingang <chenjingang@yushu.biz>
2022-11-10 10:22:55 +0800

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author chenjingang@yushu.biz
+ * @date ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
